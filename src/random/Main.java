88fdc1ddea9031937fd7945600fe3a95975a83d2
Артем <zqq1111@yandex.ru>
2020-03-05 23:09:29 +0400

    класс скан

---
+++ src/random/Main.java
@@ -0,0 +1,13 @@
+package random;
+
+import java.util.Scanner;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Scanner scanner = new Scanner(System.in);
+        String a = scanner.nextLine();
+    }
+
+}
