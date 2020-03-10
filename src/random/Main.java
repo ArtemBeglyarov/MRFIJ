44c75bae95e77144f2f73290cf3d74a119af03cc
Артем <zqq1111@yandex.ru>
2020-03-10 06:59:10 +0400

    array charq

--- src/random/Main.java
+++ src/random/Main.java
@@ -8,6 +8,18 @@ public class Main
     {
         Scanner scanner = new Scanner(System.in);
         String a = scanner.nextLine();
-    }
 
+        char [] roman = {'I', 'V', 'X', 'L', 'D', 'M'};
+        char[] array = a.toCharArray();
+        for (int i = 0; i < array.length; i++)
+        {
+
+
+
+                    System.out.println("некорректный ввод");
+            }
+        }
+    }
 }
+
+
