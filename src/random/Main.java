487352fd130c301b5ae4064fb2312720add607f8
TEMA-TASHIT666 <zqq1111@yandex.ru>
2020-03-17 10:26:16 +0400

    поптыка конвертировать 1\5\10

--- src/random/Main.java
+++ src/random/Main.java
@@ -1,6 +1,9 @@
 package random;
 
+import java.nio.channels.NotYetBoundException;
 import java.util.Scanner;
+
+
 
 public class Main
 {
@@ -36,7 +39,118 @@ public class Main
             {
                 number[i] = 1000;
             }
-            System.out.println(number[i]);
+        }
+        for (int i = 0; i < number.length; i++)
+        {
+            if (number[0] == 1)
+            {
+                if (number.length == 1)
+                {
+                    System.out.println(number[0]);
+                    break;
+                }
+                if (number.length > 1)
+                {
+                    if (number.length == 2)
+                    {
+                        if (number[1] == 5)
+                        {
+                            System.out.println(number[1] - number[0]);
+                            break;
+                        }
+                        if (number[1] == 10)
+                        {
+                            System.out.println(number[1] - number[0]);
+                            break;
+                        }
+                        if (number[0] == 1 && number[1] == 1)
+                        {
+                            System.out.println(number[0] + number[1]);
+                            break;
+                        }
+
+                    }
+                    if (number.length == 3)
+                    {
+                        if (number[0] == 1 && number[1] == 1 && number[2] == 1)
+                        {
+                            System.out.println(number[0] + number[1] + number[2]);
+                            break;
+                        }
+
+                    }
+                    else
+                    {
+                        System.out.println("error");
+                        break;
+                    }
+                }
+
+            }
+            if (number[0] == 5)
+            {
+                if (number.length == 1)
+                {
+                    System.out.println(number[0]);
+                    break;
+                }
+                if (number.length > 1)
+                {
+                    if (number.length == 2 && number[1] == 1)
+                    {
+                        System.out.println(number[0] + number[1]);
+                        break;
+                    }
+                    if (number.length == 3 && number[1]== 1 && number[2] == 1)
+                    {
+                        System.out.println(number[0]+number[1]+number[2]);
+                        break;
+                    }
+                    if (number.length == 4 && number[1] == 1 && number[2] == 1 && number[3] == 1)
+                    {
+                        System.out.println(number[0] + number[1] + number[2] + number[3]);
+                        break;
+                    }
+                    else
+                    {
+                        System.out.println("error");
+                        break;
+                    }
+
+                }
+            }
+            if (number[0] == 10)
+            {
+                if (number.length == 1)
+                {
+                    System.out.println(number[0]);
+                    break;
+                }
+                if (number.length == 2)
+                {
+                    if (number[1] == 50)
+                    {
+
+                        System.out.println(number[1] - number[0]);
+                        break;
+                    }
+                    else if (number[1] == 100 || number[1] == 1000)
+                    {
+                        System.out.println("error");
+                    }
+                    else
+                    {
+                        System.out.println(number[0] + number[1]);
+                    }
+                    break;
+                }
+                if (number.length == 3)
+                {
+
+                }
+
+            }
+
         }
 
 
