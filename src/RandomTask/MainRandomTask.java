1fe06963dd8da0f4d813b8c1bd8bc224f9ac0e44
MRFIJ <zqq1111@yandex.ru>
2020-09-07 12:33:49 +0400

    переделанный код

--- src/RandomTask/MainRandomTask.java
+++ src/RandomTask/MainRandomTask.java
@@ -11,11 +11,12 @@ import java.util.HashMap;
 import java.util.Map;
 
 
+
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.transform.OutputKeys;
 
+import javax.xml.transform.OutputKeys;
 import javax.xml.transform.Transformer;
 import javax.xml.transform.TransformerException;
 import javax.xml.transform.TransformerFactory;
@@ -23,27 +24,26 @@ import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
 
 
-public class MainRandomTask
-{
+
+
+public class MainRandomTask {
 
 
     public static void main(String[] args)
             throws ParserConfigurationException, TransformerException, FileNotFoundException,
-            UnsupportedEncodingException
-    {
+            UnsupportedEncodingException {
 
-        if (args.length % 2 == 0)
-        {
+        if (args.length % 2 == 0) {
 
 
             Map<String, String> map = new HashMap<>();
 
-            for (int i = 0; i < args.length; i += 2)
-            {
+            for (int i = 0; i < args.length; i += 2) {
 
                 map.put(args[i], args[i + 1]);
 
             }
+
 
 
             DocumentBuilderFactory factory = DocumentBuilderFactory.newDefaultInstance();
@@ -54,12 +54,18 @@ public class MainRandomTask
 
             document.appendChild(gen);
 
-            if (map.containsKey("-c"))
-            {
-                int count = Integer.parseInt(map.get("-c"));
+            System.out.println(map.get("-c"));
+            System.out.println(map.get("-f"));
 
-                for (int i = 0; i < count; i++)
-                {
+
+            if (map.containsKey("-c")) {
+
+                String size =  map.get("-c");
+                int count = Integer.parseInt(map.get("-c"));
+                System.out.println(count);
+
+
+                for (int i = 0; i < count; i++) {
 
 
                     Element root = document.createElement("person");
@@ -78,234 +84,121 @@ public class MainRandomTask
                     birthDay.setTextContent(Data.birthday());
 
 
+
                 }
             }
 
-            if (map.containsKey("-s"))
-            {
-                String size = (map.get("-s"));
+            if (map.containsKey("-s")) {
+
+                String size = map.get("-s");
+
 
                 int sum = 0;
-
-                System.out.println(size.indexOf("kmgb"));
-
-                if (size.endsWith("kb"))
-                {
-
-                    int sizeInt = Integer.parseInt(size.substring(0, size.length() - 2)) * 1000;
+                int result=0;
 
 
-                    if (sizeInt > 2000000000)
-                    {
-                        System.out.println("превышен размер файла");
-                    }
-                    else
-
-                    {
-                        for (int i = 0; i < sizeInt; i++)
-                        {
 
 
-                            Element root = document.createElement("person");
-                            Element firstName = document.createElement("firstName");
-                            Element secondName = document.createElement("secondName");
-                            Element birthDay = document.createElement("birthDay");
 
 
-                            gen.appendChild(root);
-                            root.appendChild(firstName);
-                            root.appendChild(secondName);
-                            root.appendChild(birthDay);
+                for (int i = 0; i <sizeInt(size,result); i++) {
 
 
-                            firstName.setTextContent(Data.firstName());
-                            secondName.setTextContent(Data.secondName());
-                            birthDay.setTextContent(Data.birthday());
-
-                            byte[] byteFirstName = Data.firstName().getBytes("UTF-8");
-                            byte[] byteSecondName = Data.secondName().getBytes("UTF-8");
-                            byte[] byteBirthDay = Data.birthday().getBytes("UTF-8");
-
-                            sum += byteFirstName.length + byteSecondName.length + byteBirthDay.length + 128;
-
-                            if (sizeInt - 71 <= sum)
-                            {
-                                break;
-                            }
+                    Element root = document.createElement("person");
+                    Element firstName = document.createElement("firstName");
+                    Element secondName = document.createElement("secondName");
+                    Element birthDay = document.createElement("birthDay");
 
 
-                        }
+                    gen.appendChild(root);
+                    root.appendChild(firstName);
+                    root.appendChild(secondName);
+                    root.appendChild(birthDay);
+
+
+                    firstName.setTextContent(Data.firstName());
+                    secondName.setTextContent(Data.secondName());
+                    birthDay.setTextContent(Data.birthday());
+
+                    byte[] byteFirstName = Data.firstName().getBytes("UTF-8");
+                    byte[] byteSecondName = Data.secondName().getBytes("UTF-8");
+                    byte[] byteBirthDay = Data.birthday().getBytes("UTF-8");
+
+                    sum += byteFirstName.length + byteSecondName.length + byteBirthDay.length + 128;
+
+                    if (sizeInt(size,result)- 71 <= sum) {
+                        break;
                     }
                 }
 
-                if (size.endsWith("mb"))
-                {
 
 
-                    int sizeInt = Integer.parseInt(size.substring(0, size.length() - 2)) * 1000000;//mb
-
-                    if (sizeInt > 2000000000)
-                    {
-                        System.out.println("превышен размер файла");
-                    }
-
-                    for (int i = 0; i < sizeInt; i++)
-                    {
 
 
-                        Element root = document.createElement("person");
-                        Element firstName = document.createElement("firstName");
-                        Element secondName = document.createElement("secondName");
-                        Element birthDay = document.createElement("birthDay");
+                if (map.containsKey("-f") == true) {
 
-
-                        gen.appendChild(root);
-                        root.appendChild(firstName);
-                        root.appendChild(secondName);
-                        root.appendChild(birthDay);
-
-
-                        firstName.setTextContent(Data.firstName());
-                        secondName.setTextContent(Data.secondName());
-                        birthDay.setTextContent(Data.birthday());
-
-                        byte[] byteFirstName = Data.firstName().getBytes("UTF-8");
-                        byte[] byteSecondName = Data.secondName().getBytes("UTF-8");
-                        byte[] byteBirthDay = Data.birthday().getBytes("UTF-8");
-
-                        sum += byteFirstName.length + byteSecondName.length + byteBirthDay.length + 128;
-
-
-                        if (sizeInt - 71 <= sum)
-                        {
-                            break;
-                        }
-
-
-                    }
-
+                    Transformer t = TransformerFactory.newDefaultInstance().newTransformer();
+                    t.setOutputProperty(OutputKeys.INDENT, "yes");
+                    t.transform(new DOMSource(document), new StreamResult(new FileOutputStream(map.get("-f"))));
                 }
-
-                if (size.endsWith("gb"))
-                {
-
-
-                    int sizeInt = Integer.parseInt(size.substring(0, size.length() - 2)) * 1000000000;//gb
-
-                    if (sizeInt > 2000000000)
-                    {
-                        System.out.println("превышен размер файла");
-                    }
-                    else {
-
-                        for (int i = 0; i < sizeInt; i++)
-                        {
-
-
-                            Element root = document.createElement("person");
-                            Element firstName = document.createElement("firstName");
-                            Element secondName = document.createElement("secondName");
-                            Element birthDay = document.createElement("birthDay");
-
-
-                            gen.appendChild(root);
-                            root.appendChild(firstName);
-                            root.appendChild(secondName);
-                            root.appendChild(birthDay);
-
-
-                            firstName.setTextContent(Data.firstName());
-                            secondName.setTextContent(Data.secondName());
-                            birthDay.setTextContent(Data.birthday());
-
-                            byte[] byteFirstName = Data.firstName().getBytes("UTF-8");
-                            byte[] byteSecondName = Data.secondName().getBytes("UTF-8");
-                            byte[] byteBirthDay = Data.birthday().getBytes("UTF-8");
-
-                            sum += byteFirstName.length + byteSecondName.length + byteBirthDay.length + 128;
-
-
-                            if (sizeInt - 71 <= sum)
-                            {
-                                break;
-                            }
-
-
-                        }
-                    }
-
+                else {
+                    Transformer t = TransformerFactory.newDefaultInstance().newTransformer();
+                    t.setOutputProperty(OutputKeys.INDENT, "yes");
+                    t.transform(new DOMSource(document), new StreamResult(new FileOutputStream("output.xml")));
                 }
-                if (size.indexOf("kmgb")==0)
-                {
-
-
-                    int sizeInt = Integer.parseInt(size);
-
-                    if (sizeInt < 260)
-                    {
-                        System.out.println("нужно увеличить размер файла");
-                    }
-                    else
-                    {
-                        for (int i = 0; i < sizeInt; i++)
-                        {
-
-
-                            Element root = document.createElement("person");
-                            Element firstName = document.createElement("firstName");
-                            Element secondName = document.createElement("secondName");
-                            Element birthDay = document.createElement("birthDay");
-
-
-                            gen.appendChild(root);
-                            root.appendChild(firstName);
-                            root.appendChild(secondName);
-                            root.appendChild(birthDay);
-
-
-                            firstName.setTextContent(Data.firstName());
-                            secondName.setTextContent(Data.secondName());
-                            birthDay.setTextContent(Data.birthday());
-
-                            byte[] byteFirstName = Data.firstName().getBytes("UTF-8");
-                            byte[] byteSecondName = Data.secondName().getBytes("UTF-8");
-                            byte[] byteBirthDay = Data.birthday().getBytes("UTF-8");
-
-                            sum += byteFirstName.length + byteSecondName.length + byteBirthDay.length + 128;
-
-
-                            if (sizeInt - 71 <= sum)
-                            {
-                                break;
-                            }
-                        }
-                    }
-
-                }
-
             }
 
-            if (map.containsKey("-f") == true)
-            {
 
-                Transformer t = TransformerFactory.newDefaultInstance().newTransformer();
-                t.setOutputProperty(OutputKeys.INDENT, "yes");
-                t.transform(new DOMSource(document), new StreamResult(new FileOutputStream(map.get("-f"))));
+            else {
+                System.out.println("некорректные аргументы");
             }
-            else
-            {
-                Transformer t = TransformerFactory.newDefaultInstance().newTransformer();
-                t.setOutputProperty(OutputKeys.INDENT, "yes");
-                t.transform(new DOMSource(document), new StreamResult(new FileOutputStream("output.xml")));
-            }
+
         }
-
-
-        else
-        {
-            System.out.println("некорректные аргументы");
-        }
-
     }
-}
 
+    public static int  sizeInt(String size, int result) {
+
+        String sizeByte = size.substring(size.length() - 2);
+
+        switch (sizeByte) {
+            case "kb":
+                String sizeNumeralKB = size.substring(0, size.length() - 2);
+
+               result = Integer.parseInt(sizeNumeralKB) * 1000;
+
+
+
+                 break;
+
+            case "mb":
+                String sizeNumeralMB = size.substring(0, size.length() - 2);
+
+                result = Integer.parseInt(sizeNumeralMB) * 1000000;
+
+
+                  break;
+
+            case "gb":
+                String sizeNumeralGB = size.substring(0, size.length() - 2);
+
+                 result = Integer.parseInt(sizeNumeralGB) * 1000000000;
+
+                  break;
+
+
+            default:
+
+                result = Integer.parseInt(size);
+
+                if (result < 260) {
+
+                    System.out.println("слишком маленькое число");
+                }
+                else
+
+                    break;
+        }
+        return result;
+    }
+
+}
