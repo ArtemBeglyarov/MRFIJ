d59f450bb1e2e05963047d4f699135907a8470bd
MRFIJ <zqq1111@yandex.ru>
2020-10-12 15:17:27 +0400

    начало 3

--- NC-TASK/src/Task3/OfficeFloor.java
+++ NC-TASK/src/Task3/OfficeFloor.java
@@ -2,14 +2,14 @@ package Task3;
 
 
 
+
 public class OfficeFloor {
 
+    Office[] offices;
 
-    int quantityOffice;
-
-    public OfficeFloor next;
-
-
+    private Node first;
+    private Node last;
+    private int quantityOffice;
 
     public int getQuantityOffice() {
         return quantityOffice;
@@ -17,19 +17,62 @@ public class OfficeFloor {
 
 
 
-
+    public OfficeFloor(Office[] offices) {
+    }
 
     public OfficeFloor(int quantityOffice) {
         this.quantityOffice = quantityOffice;
 
+    }
+
+    private void addNode(Office office ) {
+        Node node = new Node(office,null);
 
     }
-    public OfficeFloor(Office [] offices) {
+
+    private void removeNode(int num) {
+
+    }
+    private getNode(int num) {
+
+    }
+    public int getAllAreaOfficeFloor() {
+
+    }
+    public int getAllRoomOfficeFloor() {
+
+    }
+
+    public Office[] getOffices() {
+        return offices;
+    }
+    public getOffice(int numOffice) {
+
+    }
+    public setOffice(int numOffice) {
+
+    }
+    public addNewOffice(int numOffice) {
+
+    }
+    public removeOffice(int numOffice) {
+
+    }
+    public getBestSpace() {
+
+}
+
+
+    public static class Node {
+        private Office office;
+        private Node next;
+
+        public Node(Office office, Node next) {
+            this.office = office;
+            this.next = next;
 
         }
 
     }
 
-
 }
-
