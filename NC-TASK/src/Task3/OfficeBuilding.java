8d1d0cb1432e879c155877cf261a21e9212c6196
MRFIJ <zqq1111@yandex.ru>
2020-10-22 16:49:44 +0400

    3 лаборатория 3 задание

--- NC-TASK/src/Task3/OfficeBuilding.java
+++ NC-TASK/src/Task3/OfficeBuilding.java
@@ -1,19 +1,46 @@
 package Task3;
 
+
+
 public class OfficeBuilding {
 
     public static class Node {
 
-        private OfficeFloor office;
+        private OfficeFloor floor;
         private Node next;
         private Node prev;
 
 
-        public Node(OfficeFloor officeFloor, Node next, Node prev) {
-            this.office = officeFloor;
+        public Node(OfficeFloor floor, Node next, Node prev) {
+            this.floor = floor;
             this.next = next;
             this.prev = prev;
         }
 
+    }
+
+    private Node head;
+    private int countFloor;
+    private Node getNode(int officesNumber) {
+        Node buf = head;
+        for (int i = 1; i < officesNumber; i++) {
+            buf = buf.next;
+        }
+        return buf;
+
+    }
+    public OfficeBuilding(Office[] flat , int countFloor) {
+        head = new Node(new OfficeFloor(flat),null,null);
+        head.next = head;
+        countFloor = 1;
+       Node current = head;
+
+        while (officesNumber != countFloor) {
+            OfficeFloor.Node node = new OfficeFloor.Node(new OfficeFloor(), head);
+            current.next = node;
+            current = node;
+
+            countOffices++;
+        }
     }
 }
