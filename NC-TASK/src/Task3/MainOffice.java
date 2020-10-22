72d0a0aff0eb02ea34956c86b2b2a1cc06057a97
MRFIJ <56173032+MRFIJ@users.noreply.github.com>
2020-10-22 21:05:00 +0400

    3 лаба 3 задание

--- NC-TASK/src/Task3/MainOffice.java
+++ NC-TASK/src/Task3/MainOffice.java
@@ -1,24 +1,40 @@
 package Task3;
 
 
+import Task2.buildings.DwellingFloor;
+
 public class MainOffice {
     public static void main(String[] args) {
         Office office1 = new Office(30,4);
-        Office office2 = new Office(14,3);
-        Office office3 = new Office(14,2);
-        Office office4 = new Office(14,11);
+        Office office2 = new Office(45,3);
+        Office office3 = new Office(154,2);
+        Office office4 = new Office(145,11);
+        Office office5 = new Office(34,11);
 
-        Office [] off = {office1,office2,office3,office4};
+        Office office6 = new Office(34,3);
+        Office office7 = new Office(155,1);
+        Office office8 = new Office(15,12);
+        Office office9 = new Office(141,4);
+        Office office10 = new Office(14,5);
 
-        OfficeFloor officeFloor = new OfficeFloor(off);
-        officeFloor.removeNode(1);
-        for (int i = 0; i <officeFloor.getCountOffices() ; i++) {
-            System.out.println(officeFloor.getOffice(i));
-        }
+        Office office11 = new Office(123,6);
+        Office office12 = new Office(43,3);
+        Office office13 = new Office(43,2);
+        Office office14 = new Office(34,3);
+        Office office15 = new Office(53,1);
+
+        Office [] off = {office1,office2,office3,office4,office5};
+        Office [] off1 = {office6,office7,office8,office9,office10};
+        Office [] off2 = {office11,office12,office13,office14,office15};
+
+        OfficeFloor officeFloor1 = new OfficeFloor(off);
+        OfficeFloor officeFloor2 = new OfficeFloor(off1);
+        OfficeFloor officeFloor3 = new OfficeFloor(off2);
+
+        OfficeFloor[] floors = {officeFloor1,officeFloor2,officeFloor3};
 
 
-
-
+        OfficeBuilding officeBuilding1 = new OfficeBuilding(4);
 
         }
     }
--- NC-TASK/src/Task3/OfficeBuilding.java
+++ NC-TASK/src/Task3/OfficeBuilding.java
@@ -21,26 +21,59 @@ public class OfficeBuilding {
 
     private Node head;
     private int countFloor;
-    private Node getNode(int officesNumber) {
+    private Node prev;
+
+    private Node getNode(int floorNumber) {
         Node buf = head;
-        for (int i = 1; i < officesNumber; i++) {
+        for (int i = 1; i < floorNumber; i++) {
             buf = buf.next;
         }
         return buf;
-
     }
-    public OfficeBuilding(Office[] flat , int countFloor) {
-        head = new Node(new OfficeFloor(flat),null,null);
+    private void addNode(int floorNumber) {
+        Node buf = head;
+        for (int i = 1; i < floorNumber; i++) {
+            buf = buf.next;
+        }
+    }
+    private void removeNode(Node node, int floorNumber) {
+        Node buf = head;
+        for (int i = 1; i < floorNumber; i++) {
+            buf = buf.next;
+        }
+    }
+
+    public OfficeBuilding(OfficeFloor[] floor) {
+        head = new Node(floor[0],null,null);
         head.next = head;
+        head.prev =head;
         countFloor = 1;
+
        Node current = head;
 
-        while (officesNumber != countFloor) {
-            OfficeFloor.Node node = new OfficeFloor.Node(new OfficeFloor(), head);
+        for (int i = 1; i <floor.length ; i++) {
+            Node node = new Node(floor[i], head,prev);
             current.next = node;
             current = node;
+            current.prev =current;
 
-            countOffices++;
+            countFloor++;
+        }
+    }
+    public  OfficeBuilding(int floorNumber) {
+        head =  new Node(new OfficeFloor(5),null,null);
+        head.next = head;
+        head.prev = head;
+        countFloor = 1;
+
+        Node current = head;
+        while (floorNumber != countFloor) {
+            Node node = new Node(new OfficeFloor(5), head, prev);
+            current.next=node;
+            current = node;
+            current.prev = current;
+
+            countFloor++;
         }
     }
 }
