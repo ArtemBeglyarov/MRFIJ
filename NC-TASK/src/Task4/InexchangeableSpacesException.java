cc8b6bbca9ee25fb641921568528977bd63d8c37
MRFIJ <zqq1111@yandex.ru>
2020-11-02 16:32:59 +0400

    3 лаборатория 6 задание

--- NC-TASK/src/Task4/InexchangeableSpacesException.java
+++ NC-TASK/src/Task4/InexchangeableSpacesException.java
@@ -1,4 +1,4 @@
 package Task4;
 
-public class InexchangeableSpacesException {
+public class InexchangeableSpacesException  {
 }
--- NC-TASK/src/Task4/PlacementExchanger.java
+++ NC-TASK/src/Task4/PlacementExchanger.java
@@ -1,4 +1,9 @@
 package Task4;
 
+import Task3.Space;
+
 public class PlacementExchanger {
+    public boolean checkExchangeSpace(Space) {
+
+    }
 }
