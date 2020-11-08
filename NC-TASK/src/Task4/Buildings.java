e528af383d219b7a2d32ad035279665df9e12604
MRFIJ <56173032+MRFIJ@users.noreply.github.com>
2020-11-08 22:34:51 +0400

    4 лаба

--- NC-TASK/src/Task4/Buildings.java
+++ NC-TASK/src/Task4/Buildings.java
@@ -3,13 +3,20 @@ import Task3.Space;
 import Task3.Floor;
 import Task3.Building;
 
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.io.Reader;
-import java.io.Writer;
+import java.io.*;
 
 public class Buildings {
-    public static void outputBuilding(Building building, OutputStream out) {
+    public static void outputBuilding(Building building, OutputStream out) throws IOException {
+        DataOutputStream data = new DataOutputStream(out);
+        data.writeInt(building.getCountFloor());
+        for (int i = 0; i <building.getCountFloor() ; i++) {
+            data.writeInt(building.getFloorByNum(i).getCountSpaceOnFloor());
+            for (int j = 0; j < building.getFloorByNum(i).getCountSpaceOnFloor(); j++) {
+                data.writeInt(building.getFloorByNum(i).getSpaceFloorNum(j).getRoom());
+                data.writeDouble(building.getFloorByNum(i).getSpaceFloorNum(j).getArea());
+            }
+
+        }
 
     }
     public static Building inputBuilding (InputStream in) {
