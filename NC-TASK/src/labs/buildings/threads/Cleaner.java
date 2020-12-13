d0062d0573589008c89bd16ab98a032179bd1982
MRFIJ <56173032+MRFIJ@users.noreply.github.com>
2020-12-13 22:04:12 +0400

    Lab_7

--- NC-TASK/src/labs/buildings/threads/Cleaner.java
+++ NC-TASK/src/labs/buildings/threads/Cleaner.java
@@ -1,4 +1,20 @@
 package labs.buildings.threads;
 
-public class Cleaner extends Thread{
+import labs.inter.Floor;
+import labs.inter.Space;
+
+public class Cleaner extends Thread {
+    private Floor floor;
+
+    public Cleaner(Floor floor) {
+        this.floor = floor;
+    }
+
+    @Override
+    public void run() {
+        Space[] spaces = floor.getArrayFloor();
+        for (int i = 0; i < spaces.length; i++) {
+            System.out.println("«Cleaning space number" + i + " with total area" + spaces[i].getArea() + "square meters».");
+        }
+    }
 }
